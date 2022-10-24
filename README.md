687cfb5aed25126c0b6ee4891d781ae590305ff2
Jacques Roux <jacquesf.roux@gmail.com>
2022-10-24 16:55:03 +0200

    Initial project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Socket Programming
+
+# This project is about programming a server and client using the socket and threading modules.
---
+++ server.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+  print(f"[NEW CONNECTION] {addr} connected.")
+
+  connected = True
+  while connected:
+    msg_length = conn.recv(HEADER).decode(FORMAT)
+    msg_length = int(msg_length)
+    msg = conn.recv(msg_length).decode(FORMAT)
+    print(f"[{addr}] {msg}")
+
+
+def start():
+  server.listen()
+  while True:
+    conn, addr = server.accept()
+    thread = threading.Thread(target=handle_client, args=(conn, addr))
+    thread.start()
+    print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+print("[STARTING] server is starting...")
+start()
